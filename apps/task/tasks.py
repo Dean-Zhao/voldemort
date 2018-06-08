c8dc438ae045f9fee056c5f7f9cc3593fa8734e7
tcreditTesters <37927717+tcreditTesters@users.noreply.github.com>
2018-06-08 16:33:41 +0800

    添加结果验证

--- apps/task/tasks.py
+++ apps/task/tasks.py
@@ -1,21 +1,27 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from .models import *
-from api.test_views import save_result,test_case,save_exception
+from api.test_views import *
+import json
+from api.models import *
 
 @shared_task
 def execute(task_id):
-    t = task.objects.get(id=task_id)
+    t = task.objects.get(id=int(task_id))
     p = t.plan
     env = t.runtime_env
     for case in p.cases.all():
+        if case.validation == '':
+            valids = json.loads('{}')
+        else:
+            valids = json.loads(case.validation)
         try:
             r = test_case(env.id, case)
-            save_result(r, case, task_id, env.id)
+            result_id = save_result(r, case, task_id, env.id)
+            if len(valids.keys()) > 0:
+                verify(result_id, valids)
         except Exception as e:
-            print "Exception...."
-            print e.message
-            save_exception(e, case, task_id, env.id)
+            result_id = save_exception(e, case, task_id, env.id)
             continue
 
     t.status = 1
